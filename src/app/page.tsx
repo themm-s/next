742350dec34171e7891e46b801f6cbca5523508d
themms <maximusnewera@mail.ru>
2023-09-07 19:48:21 +0600

    neon button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Home() {
   return (
     <>
-      <nav className="p-2 h-screen w-80 font-bold">
+      <nav className="p-2 h-screen w-96 font-bold">
         <ul className="mt-72 pl-5 space-y-6">
-          <li><a href="/dashboard"><FontAwesomeIcon icon={faFileLines} className="w-5 text-center" />Dashboard</a></li>
-          <li><a href="/perfomance"><FontAwesomeIcon icon={faChartSimple} className="w-5 text-center" />Shot Perfomance</a></li>
-          <li><a href="/followers"><FontAwesomeIcon icon={faFaceLaugh} className="w-5 text-center" />Followers</a></li>
-          <li><a href="/apanel"><FontAwesomeIcon icon={faChartPie} className="w-5 text-center" />Admin Panel</a></li>
+          <li><a href="/dashboard"><FontAwesomeIcon icon={faFileLines} className="w-6 text-center" />Dashboard</a></li>
+          <li><a href="/perfomance"><FontAwesomeIcon icon={faChartSimple} className="w-6 text-center" />Perfomance</a></li>
+          <li><a href="/followers"><FontAwesomeIcon icon={faFaceLaugh} className="w-6 text-center" />Followers</a></li>
+          <li><a href="/apanel"><FontAwesomeIcon icon={faChartPie} className="w-6 text-center" />Admin Panel</a></li>
         </ul>
       </nav >
     </>
